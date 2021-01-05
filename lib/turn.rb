692a445beb3f024caf4380f7f9c5f3c0a368da6b
Sam D <samidiaz3@gmail.com>
2021-01-05 23:25:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,3 +23,12 @@ end
 	    #true - position taked
 	  end
 	end
+
+
+#def input_to_index (user_input)
+#  user_input.to_i - 1
+#end
+
+def move (board, index, player = "X")
+  board[index] = player
+end
