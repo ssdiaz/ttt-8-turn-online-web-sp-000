bb1208965299826abe56da753a69c5e8e63668ac
Sam D <samidiaz3@gmail.com>
2021-01-05 23:20:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,11 @@ def display_board (board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+  if !position_taken?(board,index) && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
