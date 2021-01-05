ebf0a07bb21679167394a62383ae5dc59e38c7a7
Sam D <samidiaz3@gmail.com>
2021-01-05 23:25:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,9 +25,9 @@ end
 	end
 
 
-#def input_to_index (user_input)
-#  user_input.to_i - 1
-#end
+def input_to_index (index)
+  index.to_i - 1
+end
 
 def move (board, index, player = "X")
   board[index] = player
